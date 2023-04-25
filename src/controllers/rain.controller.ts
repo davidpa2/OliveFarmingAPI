7d1ea734d17f5b1d5a53fea9a837ef419fb8301f
David Padilla <davidpaag2002@gmail.com>
2023-04-25 16:29:45 +0200

    feat(rain): Two first endpoints to get and to post

--- src/controllers/rain.controller.ts
+++ src/controllers/rain.controller.ts
@@ -1,8 +1,53 @@
 // Uncomment these imports to begin using these cool features!
 
+import {Filter, repository} from '@loopback/repository';
+import {get, getModelSchemaRef, param, post, requestBody, response} from '@loopback/rest';
+import {Rain} from '../models';
+import {RainRepository} from '../repositories';
+
 // import {inject} from '@loopback/core';
 
 
 export class RainController {
-  constructor() {}
+  constructor(
+    @repository(RainRepository) protected rainRepository: RainRepository,
+  ) { }
+
+  @get('/rain')
+  @response(200, {
+    description: 'Array of User model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Rain, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Rain) filter?: Filter<Rain>,
+  ): Promise<Rain[]> {
+    return this.rainRepository.find(filter);
+  }
+
+  @post('/rain')
+  @response(200, {
+    description: 'Rain model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Rain)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Rain, {
+            title: 'New rain log',
+          }),
+        },
+      },
+    })
+    rain: Rain,
+  ): Promise<Rain> {
+    return this.rainRepository.create(rain);
+  }
 }
